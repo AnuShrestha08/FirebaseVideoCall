a5a9be699f062847f57a9f199a626e860ba05c62
AnuShrestha08 <swifttechanushrestha@gmail.com>
2023-06-09 12:20:14 +0545

    solved till epi2

--- app/src/main/java/com/anu/firebasevideocallingapp/activities/SignInActivity.kt
+++ app/src/main/java/com/anu/firebasevideocallingapp/activities/SignInActivity.kt
@@ -4,9 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+//import android.widget.Toast
 import com.anu.firebasevideocallingapp.R
+//import com.google.firebase.firestore.FirebaseFirestore
 
 class SignInActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -15,6 +20,21 @@ class SignInActivity : AppCompatActivity() {
         textSignUp.setOnClickListener{
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
+
+// code written for testing purpose only so that it can be seen in firebase
+//            val database: FirebaseFirestore = FirebaseFirestore.getInstance()
+//            val user: HashMap<String, Any> = HashMap()
+//            user["first_name"] = "Anu"
+//            user["last_name"] = "Shrestha"
+//            user["email"] = "anu.shrestha@gmail.com"
+//            database.collection("users")
+//                .add(user)
+//                .addOnSuccessListener { documentReference ->
+//                    Toast.makeText(this@SignInActivity, "User Inserted", Toast.LENGTH_SHORT).show()
+//                }
+//                .addOnFailureListener { e ->
+//                    Toast.makeText(this@SignInActivity, "User Failed to Insert", Toast.LENGTH_SHORT).show()
+//                }
         }
 
     }
--- app/src/main/java/com/anu/firebasevideocallingapp/firebase/MessagingService.kt
+++ app/src/main/java/com/anu/firebasevideocallingapp/firebase/MessagingService.kt
@@ -1,6 +1,6 @@
 package com.anu.firebasevideocallingapp.firebase
 
-import android.util.Log
+//import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -8,13 +8,13 @@ class MessagingService : FirebaseMessagingService (){
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Log.d("FCM","Token : $token")
+//        Log.d("FCM","Token : $token")
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-        message.notification?.let{ notification ->
-            Log.d("FCM", "Remote message received: ${notification.body}")
-        }
+//        message.notification?.let{ notification ->
+//            Log.d("FCM", "Remote message received: ${notification.body}")
+//        }
     }
 }
