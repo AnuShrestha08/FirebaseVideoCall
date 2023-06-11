7d9e3ea4ecb7e4e8472f543934b2ed9dfccc7138
AnuShrestha08 <swifttechanushrestha@gmail.com>
2023-06-11 17:03:51 +0545

    ellipsize attribute to be added

---
+++ app/src/main/java/com/anu/firebasevideocallingapp/models/User.kt
@@ -0,0 +1,12 @@
+package com.anu.firebasevideocallingapp.models
+
+import java.io.Serializable
+
+class User : Serializable{
+     lateinit var firstName : String
+     lateinit var LastName : String
+     lateinit var email : String
+     lateinit var token : String
+
+
+}
