df058fafa1ffb896306ae567d5d8f28a45095428
AnuShrestha08 <swiftechanushrestha@gmail.com>
2023-06-15 06:29:07 +0545

    List of users shown while logging from other user

--- app/src/main/java/com/anu/firebasevideocallingapp/activities/MainActivity.kt
+++ app/src/main/java/com/anu/firebasevideocallingapp/activities/MainActivity.kt
@@ -16,11 +16,13 @@ import com.anu.firebasevideocallingapp.utilities.PreferenceManager
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessaging
+import com.google.android.gms.tasks.OnCompleteListener
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var preferenceManager: PreferenceManager
-    private lateinit var users: List<User>
+    private lateinit var users: MutableList<User>
     private lateinit var usersAdapters: UsersAdapters
     private lateinit var textErrorMessage: TextView
     private lateinit var usersProgressBar: ProgressBar
@@ -60,11 +62,48 @@ class MainActivity : AppCompatActivity() {
         usersAdapters = UsersAdapters(users)
         usersRecyclerView.adapter=usersAdapters
 
+        getUsers()
+
     }
 
+    private fun getUsers() {
+        usersProgressBar.visibility = View.VISIBLE
+        val database = FirebaseFirestore.getInstance()
+        database.collection(Constants.KEY_COLLECTION_USERS)
+            .get()
+            .addOnCompleteListener { task ->
+                usersProgressBar.visibility = View.GONE
+                val myUserId = preferenceManager.getString(Constants.KEY_USERS_ID)
+                if (task.isSuccessful && task.result != null) {
+                    for (documentSnapshot in task.result!!) {
+                        if (myUserId == documentSnapshot.id) {
+                            continue
+                        }
+                        val user = User()
+                        user.firstName = documentSnapshot.getString(Constants.KEY_FIRST_NAME) ?: ""
+                        user.LastName = documentSnapshot.getString(Constants.KEY_LAST_NAME) ?: ""
+                        user.email = documentSnapshot.getString(Constants.KEY_EMAIL) ?: ""
+                        user.token = documentSnapshot.getString(Constants.KEY_FCM_TOKEN) ?: ""
+                        users.add(user)
+                    }
+
+                    if (users.size > 0) {
+                        usersAdapters.notifyDataSetChanged()
+                    } else {
+                        textErrorMessage.text = String.format("%s", "No users available")
+                        textErrorMessage.visibility = View.VISIBLE
+                    }
+                } else {
+                    textErrorMessage.text = String.format("%s", "No users available")
+                    textErrorMessage.visibility = View.VISIBLE
+                }
+            }
+        }
 
 
-    private fun sendFCMTokenToDatabase(token: String) {
+
+
+        private fun sendFCMTokenToDatabase(token: String) {
         val database = FirebaseFirestore.getInstance()
         val documentReference = database.collection(Constants.KEY_COLLECTION_USERS)
             .document(preferenceManager.getString(Constants.KEY_USERS_ID)?: "")
