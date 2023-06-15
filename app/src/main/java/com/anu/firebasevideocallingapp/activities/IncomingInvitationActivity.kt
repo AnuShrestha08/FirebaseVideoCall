9a036362c91c0f879b5904516a234837c106cfce
AnuShrestha08 <swiftechanushrestha@gmail.com>
2023-06-15 08:46:01 +0545

    Design for incoming meeting invitation

---
+++ app/src/main/java/com/anu/firebasevideocallingapp/activities/IncomingInvitationActivity.kt
@@ -0,0 +1,12 @@
+package com.anu.firebasevideocallingapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anu.firebasevideocallingapp.R
+
+class IncomingInvitationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_incoming_invitation)
+    }
+}
