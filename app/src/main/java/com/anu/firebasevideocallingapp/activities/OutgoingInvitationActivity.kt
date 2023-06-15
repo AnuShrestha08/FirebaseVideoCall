bc18d03945e77fce780c82750321261f41bc935d
AnuShrestha08 <swifttechanushrestha@gmail.com>
2023-06-15 11:36:30 +0545

    Design for Outgoing Meeting Invitation

---
+++ app/src/main/java/com/anu/firebasevideocallingapp/activities/OutgoingInvitationActivity.kt
@@ -0,0 +1,12 @@
+package com.anu.firebasevideocallingapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anu.firebasevideocallingapp.R
+
+class OutgoingInvitationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_outgoing_invitation)
+    }
+}
