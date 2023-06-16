b9cb1f054c1761420005ac0bac48e5f29a7f33fb
AnuShrestha08 <swiftechanushrestha@gmail.com>
2023-06-16 07:32:13 +0545

    Invitation sent successfully(OA)-now incoming logic left(IA)

--- app/src/main/java/com/anu/firebasevideocallingapp/activities/OutgoingInvitationActivity.kt
+++ app/src/main/java/com/anu/firebasevideocallingapp/activities/OutgoingInvitationActivity.kt
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.anu.firebasevideocallingapp.R
 import com.anu.firebasevideocallingapp.models.User
+import com.anu.firebasevideocallingapp.network.ApiClient
+import com.anu.firebasevideocallingapp.network.ApiService
+import com.anu.firebasevideocallingapp.utilities.Constants
 import com.anu.firebasevideocallingapp.utilities.PreferenceManager
+import com.google.firebase.messaging.FirebaseMessaging
+import org.json.JSONArray
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class OutgoingInvitationActivity : AppCompatActivity() {
 
@@ -49,14 +59,82 @@ class OutgoingInvitationActivity : AppCompatActivity() {
         }
 
         preferenceManager = PreferenceManager(applicationContext)
-//        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener{task->
-//            if(task.isSuccessful && task.result != null){
-//                inviterToken = task.result?.token
-//
-//            }
-//
-//        }
+        FirebaseMessaging.getInstance().token.addOnCompleteListener{ task->
+            if(task.isSuccessful && task.result != null){
+                inviterToken = task.result
+
+            }
+
+        }
+
+        if (meetingType != null && user != null) {
+            initiateMeeting(meetingType, user.token)
+        }
 
     }
+
+    private fun initiateMeeting(meetingType: String, receiverToken: String) {
+        try {
+            val tokens = JSONArray().apply { put(receiverToken) }
+
+            val body = JSONObject().apply {
+                val data = JSONObject().apply {
+                    put(Constants.REMOTE_MSG_TYPE, Constants.REMOTE_MSG_INVITATION)
+                    put(Constants.REMOTE_MSG_MEETING_TYPE, meetingType)
+                    put(Constants.KEY_FIRST_NAME, preferenceManager.getString(Constants.KEY_FIRST_NAME))
+                    put(Constants.KEY_LAST_NAME, preferenceManager.getString(Constants.KEY_LAST_NAME))
+                    put(Constants.KEY_EMAIL, preferenceManager.getString(Constants.KEY_EMAIL))
+                    put(Constants.REMOTE_MSG_INVITER_TOKEN, inviterToken)
+                }
+                put(Constants.REMOTE_MSG_DATA, data)
+                put(Constants.REMOTE_MSG_REGISTRATION_IDS, tokens)
+            }
+
+            sendRemoteMessage(body.toString(), Constants.REMOTE_MSG_INVITATION)
+
+
+        } catch (exception: Exception) {
+            Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
+
+
+    private fun sendRemoteMessage(remoteMessageBody: String, type: String) {
+        ApiClient.getClient().create(ApiService::class.java).sendRemoteMessage(
+            Constants.getRemoteMessageHeaders(),
+            remoteMessageBody
+        ).enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    if (type == Constants.REMOTE_MSG_INVITATION) {
+                        Toast.makeText(
+                            this@OutgoingInvitationActivity,
+                            "Invitation sent successfully",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            this@OutgoingInvitationActivity,
+                            response.message(),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    finish()
+                }
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Toast.makeText(
+                    this@OutgoingInvitationActivity,
+                    t.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        })
+    }
+
 
 }
--- app/src/main/java/com/anu/firebasevideocallingapp/utilities/Constants.kt
+++ app/src/main/java/com/anu/firebasevideocallingapp/utilities/Constants.kt
@@ -13,8 +13,24 @@ class Constants {
         const val KEY_USERS_ID = "users_id"
         const val KEY_FCM_TOKEN= "fcm_token"
 
+        const val REMOTE_MSG_AUTHORIZATION = "Authorization"
+        const val REMOTE_MSG_CONTENT_TYPE = "Content-type"
 
 
+        const val REMOTE_MSG_TYPE = "type"
+        const val REMOTE_MSG_INVITATION = "invitation"
+        const val REMOTE_MSG_MEETING_TYPE = "meeting-type"
+        const val REMOTE_MSG_INVITER_TOKEN = "inviterToken"
+        const val REMOTE_MSG_DATA = "data"
+        const val REMOTE_MSG_REGISTRATION_IDS= "registration_ids"
+
+        fun getRemoteMessageHeaders(): HashMap<String, String> {
+            val headers = HashMap<String, String>()
+            headers[Constants.REMOTE_MSG_AUTHORIZATION] =
+                "key=AAAAb373sbw:APA91bGrelzovy9J_UZhqNFgUzdZaRqNKRt6LIA2LcDLAoYcZ2Etxhrv41AXQqghyDLqtZeoPlJGqNWi_1HIkLdeH8Lw6yf6qaNhQ32JUpG0or2g34uuNKmpuyrqj-E4TqKl3_fdPY_j"
+            headers[Constants.REMOTE_MSG_CONTENT_TYPE] = "application/json"
+            return headers
+        }
 
     }
 
